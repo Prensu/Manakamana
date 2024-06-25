00c2d8d9e2efdfa52926090fe5774bc28da166c1
Prensu <136965950+Prensu@users.noreply.github.com>
2024-06-25 12:26:48 +0545

    Add files via upload
    nothing much just added a python simple code file

---
+++ Calculating  max, min and average.py
@@ -0,0 +1,7 @@
+list_num= []
+while True:
+    num= input("Enter a number (Enter 'done' to finish): ")
+    if num == "done":
+        break
+    list_num.append(int(num))
+print("Muximum=", max(list_num), "  Minimum=", min(list_num), "  Average:", sum(list_num)/len(list_num))
