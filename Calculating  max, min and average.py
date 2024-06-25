4a02650544fb599386c54bc5a0a6ba749e1262fb
Prensu <136965950+Prensu@users.noreply.github.com>
2024-06-25 12:52:10 +0545

    Update Calculating  max, min and average.py
    just wanna use pull request so trying  to make a new branch

--- Calculating  max, min and average.py
+++ Calculating  max, min and average.py
@@ -5,3 +5,5 @@ while True:
         break
     list_num.append(int(num))
 print("Muximum=", max(list_num), "  Minimum=", min(list_num), "  Average:", sum(list_num)/len(list_num))
+
+print("now the program is completed! ") 
